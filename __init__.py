88bb29d3f8f7d1a0a2e35e963bf0dafed81b9cf3
blenderboi <dummy@dummy.com>
2022-04-14 14:54:35 +0800

    add preset, and installs preset, added changelog wip

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "Game Rig Tools",
     "author": "BlenderBoi, Xin",
-    "version": (1, 7, 1),
+    "version": (1, 7, 2),
     "blender": (3, 1, 0),
     "description": "Generate a Deform Rig base on CGDive's Game-Ready Rig Video",
     "warning": "",
@@ -21,9 +21,36 @@ from . import Preferences
 from . import GRT_Action_Bakery
 from . import addition
 
+import os
+import pathlib
+import shutil
+
+current_path = pathlib.Path(__file__).parent
+script_directory = str(current_path.parents[1]) 
+preset_directory = os.path.join(script_directory, "presets")
+src_preset_path = os.path.join(current_path, "presets")
+
+check_dir = os.path.join(preset_directory, "operator", "gamerigtool.generate_game_rig")
+
 modules = [addition, GRT_Extra_Operators, Deform_Rig_Generator, Deform_Rig_Panel, GRT_Action_Bakery, Preferences]
 
+
 def register():
+    
+    copy_preset = True
+
+    if os.path.isdir(check_dir):
+
+        for file in os.listdir(check_dir):
+            if file.endswith(".py"):
+                copy_preset = False
+                break
+    else:
+        copy_preset = True 
+
+    if copy_preset:
+    # os.path.isdir(os.path.join(preset_directory, ""))
+        shutil.copytree(src_preset_path, preset_directory, dirs_exist_ok=True)
 
     for module in modules:
         module.register()
---
+++ presets/operator/gamerigtool.generate_game_rig/Default_-_keep_hierarchy.py
@@ -0,0 +1,25 @@
+import bpy
+op = bpy.context.active_operator
+
+op.Extract_Mode = 'DEFORM'
+op.Flat_Hierarchy = False
+op.Disconnect_Bone = True
+op.Constraint_Type = 'LOTROT'
+op.Animator_Remove_BBone = False
+op.Animator_Disable_Deform = False
+op.Parent_To_Deform_Rig = True
+op.Deform_Armature_Name = ''
+op.Deform_Remove_BBone = True
+op.Deform_Move_Bone_to_Layer1 = True
+op.Deform_Set_Inherit_Rotation_True = True
+op.Deform_Set_Inherit_Scale_Full = True
+op.Deform_Set_Local_Location_True = True
+op.Deform_Remove_Non_Deform_Bone = True
+op.Deform_Unlock_Transform = True
+op.Deform_Remove_Shape = True
+op.Deform_Remove_All_Constraints = True
+op.Deform_Copy_Transform = True
+op.Deform_Bind_to_Deform_Rig = True
+op.Remove_Custom_Properties = True
+op.Remove_Animation_Data = True
+op.Show_Advanced = False
